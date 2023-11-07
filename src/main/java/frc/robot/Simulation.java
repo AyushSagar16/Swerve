d7f7b7da30821bea5e963b78b14cbfb24ec7653c
Ayush Sagar <ayushsagar3018@gmail.com>
2023-11-06 21:33:12 -0500

    added odometry

--- src/main/java/frc/robot/Simulation.java
+++ src/main/java/frc/robot/Simulation.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,10 +22,16 @@ public class Simulation {
     // Array for the sim wheels
     private final FieldObject2d[] modules_ = {front_left_, front_right_, back_left_, back_right_};
 
-
-
     // IO
     private final IO io_ = new IO();
+
+    SwerveDriveKinematics sim_kinematics_ = new SwerveDriveKinematics(
+        Constants.front_left_location_, Constants.front_right_location_,
+        Constants.back_left_location_, Constants.back_right_location_);
+    
+    SwerveDriveOdometry sim_odometry_ = new SwerveDriveOdometry(sim_kinematics_, io_.pose_.getRotation(), null, null);
+
+    
 
 
     public Simulation() {
@@ -32,7 +41,7 @@ public class Simulation {
 
     // Add odometry to the sim
     public void update() {
-        field_.setRobotPose(5, 2, Rotation2d.fromDegrees(90));
+        field_.setRobotPose(io_.pose_);
         front_left_.setPose(5, 2, Rotation2d.fromDegrees(90));
     }
 
@@ -40,6 +49,13 @@ public class Simulation {
         // Input 
 
         // Output
-        Pose2d pose_;
+        Pose2d pose_ = new Pose2d(new Translation2d(0, 0), Rotation2d.fromDegrees(0));
+    }
+
+    public static class Constants {
+        public static final Translation2d front_left_location_ = new Translation2d(0.5, 0.5);
+        public static final Translation2d front_right_location_ = new Translation2d(0.5, -0.5);
+        public static final Translation2d back_left_location_ = new Translation2d(-0.5, 0.5);
+        public static final Translation2d back_right_location_ = new Translation2d(-0.5, -0.5);
     }
 }
